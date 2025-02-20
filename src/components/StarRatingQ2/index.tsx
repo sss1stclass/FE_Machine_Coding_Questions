b3d72f25f308c6ecb6f67686401fd3aa6e49931d
sss1stclass <sohilkr88@gmail.com>
2025-02-20 21:32:24 +0530

    removed consoles

--- src/components/StarRatingQ2/index.tsx
+++ src/components/StarRatingQ2/index.tsx
@@ -3,13 +3,11 @@ import React, { useState } from "react";
 const StarRating = ({ totalStars = 5 }) => {
   const [rating, setRating] = useState(0);
   const [hover, setHover] = useState(0);
-
-
-  console.log(rating, 'rate');
-  console.log(hover, 'hover')
-
+  
   return (
-    <div style={{ display: "flex", gap: "5px", cursor: "pointer" }}>
+    <>
+    <h1 style={{textAlign:'center'}}>Star Rating</h1>
+    <div style={{ display: "flex", alignItems:'center', justifyContent:'center', gap: "5px", cursor: "pointer" }}>
       {[...Array(totalStars)].map((_, index) => {
         const starValue = index + 1;
         return (
@@ -28,6 +26,7 @@ const StarRating = ({ totalStars = 5 }) => {
         );
       })}
     </div>
+    </>
   );
 };
 
