4a65e6eff0b5fae0fc08ec595e3d25857969be05
sss1stclass <sohilkr88@gmail.com>
2025-04-01 17:53:03 +0530

    implemented auth and login singuo

--- firebase.ts
+++ firebase.ts
@@ -1,17 +1,14 @@
-// Import the functions you need from the SDKs you need
+// firebase.ts
 import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_GOOGLE_FIREBASE_API_KEY,
+  apiKey: import.meta.env.VITE_GOOGLE_FIREBASE_API_KEY,
   authDomain: "dashboard-12f9b.firebaseapp.com",
   projectId: "dashboard-12f9b",
   storageBucket: "dashboard-12f9b.firebasestorage.app",
   messagingSenderId: "791503586859",
   appId: "1:791503586859:web:786fd9cd4edc8f9bb2cc6e",
-  databaseURL:'https://dashboard-12f9b-default-rtdb.firebaseio.com/'
+  databaseURL: 'https://dashboard-12f9b-default-rtdb.firebaseio.com/'
 };
 
 // Initialize Firebase
--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,12 @@
 // import StarRating from "./components/StarRatingQ2";
 import { app } from '../firebase';
 import { getDatabase, ref, set } from 'firebase/database';
+import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth";
 
 const App = () => {
   const db = getDatabase(app);
+  const auth = getAuth(app);
+
 
   const testApp = () => {
     set(ref(db, 'users/sohilxdfsf'), {
@@ -14,6 +17,26 @@ const App = () => {
       job: 'unemployed from oct 08 2024'
     });
   }
+
+  const getSignUP = ()=>{
+    createUserWithEmailAndPassword(auth, 'sohlilkr88@gmail.com', 'asfkss12312sdfks').then((data)=>{
+      console.log(data);
+      alert('You have sucessfully sign up');
+    })
+  }
+
+  const logIN = async ()=>{
+    try{
+      const data = await  signInWithEmailAndPassword(auth, 'sohlilkr88@gmail.com', 'asfkss12312sdfks1231')
+      if(data){
+        console.log(data.user);
+        alert('success')
+      }
+    }catch(err){
+      console.log(err,'this is error')
+    }
+  }
+
   return (
     <>
       {/* // <EmployeeDBManagement/>
@@ -23,6 +46,8 @@ const App = () => {
       </h1>
       <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
         <button style={{ textAlign: 'center' }} onClick={testApp}>click to Add Data</button>
+        <button onClick={getSignUP}>SIGN UP</button>
+        <button onClick={logIN}>Login in </button>
       </div>
 
     </>
